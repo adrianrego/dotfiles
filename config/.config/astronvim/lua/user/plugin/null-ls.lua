b7b1358942334c8a3bc7c2b9bfcfad4dfa3b41ca
adrianrego <adrian.rego@everstream.ai>
2023-04-06 21:23:49 -0400

    switch to astrovim

---
+++ config/.config/astronvim/lua/user/plugin/null-ls.lua
@@ -0,0 +1,42 @@
+return {
+  "jose-elias-alvarez/null-ls.nvim",
+  opts = function(_, config)
+    -- config variable is the default configuration table for the setup function call
+    local nls = require "null-ls"
+    local h = require "null-ls.helpers"
+
+    local gherkin = h.make_builtin({
+        name = "reformat-gherkin",
+        meta = {
+            url = "https://github.com/ducminh-phan/reformat-gherkin",
+            description = "Reformat-gherkin automatically formats Gherkin files.",
+        },
+        method = nls.methods.FORMATTING,
+        filetypes = { "cucumber" },
+        generator_opts = {
+            command = "reformat-gherkin",
+            args = {
+                "-",
+            },
+            to_stdin = true,
+        },
+        factory = h.formatter_factory,
+    })
+
+    nls.register(gherkin)
+
+    config.sources = {
+      nls.builtins.code_actions.eslint,
+      nls.builtins.code_actions.gitsigns,
+		  nls.builtins.completion.spell,
+		  nls.builtins.diagnostics.eslint,
+		  nls.builtins.diagnostics.flake8,
+		  nls.builtins.formatting.black,
+		  nls.builtins.formatting.isort,
+		  nls.builtins.formatting.prettier,
+		  nls.builtins.formatting.stylua,
+		  nls.builtins.formatting.terraform_fmt,
+    }
+    return config -- return final config table
+  end,
+}
---
+++ config/.config/astronvim/lua/user/plugin/treesitter.lua
@@ -0,0 +1,6 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = { "lua", "python", "bash", "css", "html", "javascript", "typescript", "dockerfile", "hcl", "yaml" },
+  },
+}
